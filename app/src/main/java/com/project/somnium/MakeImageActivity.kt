65e6163978f7ec546b5c1f1bad61231a99672e23
junho <junho5508@naver.com>
2025-04-10 23:59:00 +0900

    콜백 함수를 통해 리사이클러 생성 하는 코드 추가

--- app/src/main/java/com/project/somnium/MakeImageActivity.kt
+++ app/src/main/java/com/project/somnium/MakeImageActivity.kt
@@ -12,10 +12,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.project.somnium.databinding.ActivityMakeImageBinding
 import com.project.somnium.recyclerview.Adapter
 import com.project.somnium.recyclerview.RecyclerDataModel
+import com.project.somnium.retrofit.GptRequest
+
 
 class MakeImageActivity : AppCompatActivity() {
-    private lateinit var input: String
-    private lateinit var data: RecyclerDataModel
+    private lateinit var inputText: String
     private val linearLayoutManager = LinearLayoutManager(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,13 +46,19 @@ class MakeImageActivity : AppCompatActivity() {
         }
 
         binding.btnSend.setOnClickListener {
-            input = binding.etDreamInput.text.toString()
+            inputText = binding.etDreamInput.text.toString()
 
-            if (input.isNotBlank()) {
-                data = RecyclerDataModel(input, R.drawable.image3)
-                recyclerDataModel.add(data)
-                recyclerAdapter.notifyDataSetChanged()
+            if (inputText.isNotBlank()) {
                 binding.etDreamInput.text.clear()
+                Toast.makeText(this,"이미지 생성중입니다.",Toast.LENGTH_SHORT).show()
+                val request = GptRequest(inputText, object : GptRequest.ApiCallback  {
+                    override fun onDataReceived(data: RecyclerDataModel) {
+                        recyclerDataModel.add(data)
+                        recyclerAdapter.notifyDataSetChanged()
+                    }
+                })
+
+                request.gptRequest()
             } else {
                 Toast.makeText(this, "꿈 내용을 입력해주세요!", Toast.LENGTH_SHORT).show()
             }
@@ -60,4 +67,5 @@ class MakeImageActivity : AppCompatActivity() {
 
 
     }
+
 }

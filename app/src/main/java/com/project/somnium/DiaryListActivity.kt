eb0d8ef88a787c937f7e5b73b79228f83d904cd0
junho <junho5508@naver.com>
2025-04-18 00:34:18 +0900

    다이어리 목록 화면 UI 및 DB 연동 기본 구현

---
+++ app/src/main/java/com/project/somnium/DiaryListActivity.kt
@@ -0,0 +1,50 @@
+package com.project.somnium
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.project.somnium.databinding.ActivityDiaryListBinding
+import com.project.somnium.diaryDb.DataBase
+import com.project.somnium.diaryListRecycler.DiaryListAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class DiaryListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val binding = ActivityDiaryListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val db = DataBase.getDatabase(this)
+        val diaryDao = db.DiaryDataDao()
+
+        binding.DiaryInventory.apply {
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+            layoutManager = LinearLayoutManager(context)
+        }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val data = diaryDao.getAllData()
+
+            withContext(Dispatchers.Main) {
+                val adapter = DiaryListAdapter(data.toMutableList()){ diary -> withContext(Dispatchers.IO){
+                    diaryDao.deleteByTile(diary.title)
+                } }
+                binding.DiaryInventory.adapter = adapter
+            }
+        }
+    }
+}

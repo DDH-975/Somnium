e5804ed1978d6ec2258d80710cfed64575fb6b6d
junho <junho5508@naver.com>
2025-04-28 22:32:05 +0900

    썸네일 리사이클러 뷰 생성 코드 추가

--- app/src/main/java/com/project/somnium/MainActivity.kt
+++ app/src/main/java/com/project/somnium/MainActivity.kt
@@ -6,7 +6,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
 import com.project.somnium.databinding.ActivityMainBinding
+import com.project.somnium.diaryDb.DataBase
+import com.project.somnium.diary_List_Recycler.DiaryListAdapter
+import com.project.somnium.thumbnail_Recycler.ThumbnailAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +33,26 @@ class MainActivity : AppCompatActivity() {
         val goToMakeImageIntent = Intent(this@MainActivity, MakeImageActivity::class.java)
         val goToWriteDiaryIntent = Intent(this@MainActivity, WriteDiaryActivity::class.java)
         val goToReadDiaryIntent = Intent(this@MainActivity, DiaryListActivity::class.java)
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val snapHelper = LinearSnapHelper()
+
+        val db = DataBase.getDatabase(this)
+        val diaryDao = db.DiaryDataDao()
+
+        binding.recyclerThumbnail.apply {
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL))
+            setLayoutManager(layoutManager)
+        }
+
+        snapHelper.attachToRecyclerView(binding.recyclerThumbnail)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val data = diaryDao.getDataDesc()
+            withContext(Dispatchers.Main) {
+                val adapter = ThumbnailAdapter(data.toMutableList())
+                binding.recyclerThumbnail.adapter = adapter
+            }
+        }
 
         binding.btnMakeImg.setOnClickListener {
             startActivity(goToMakeImageIntent)

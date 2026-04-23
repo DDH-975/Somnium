6cf15e3f689179dadb580b20e674712cc258edf1
junho <junho5508@naver.com>
2026-04-23 22:26:45 +0900

    MakeImageViewModel 추가

---
+++ app/src/main/java/com/project/somnium/viewModel/MakeImageViewModel.kt
@@ -0,0 +1,32 @@
+package com.project.somnium.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.project.somnium.makeImg_Recycler.RecyclerDataModel
+import com.project.somnium.retrofit.GptRepository
+import kotlinx.coroutines.launch
+
+class MakeImageViewModel : ViewModel() {
+
+    private val repository = GptRepository()
+
+    private val _imageData = MutableLiveData<RecyclerDataModel>()
+    val imageData: LiveData<RecyclerDataModel> = _imageData
+
+    private val _errorMessage = MutableLiveData<String>()
+    val errorMessage: LiveData<String> = _errorMessage
+
+    fun generateImage(prompt: String) {
+        viewModelScope.launch {
+            val result = repository.requestImage(prompt)
+
+            result.onSuccess {
+                _imageData.value = it
+            }.onFailure {
+                _errorMessage.value = it.message
+            }
+        }
+    }
+}

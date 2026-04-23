ea7b286d0cdd49d03760ec556d036b47026278ca
junho <junho5508@naver.com>
2026-04-23 22:26:58 +0900

    MakeImageActivity ViewModel 적용 및 리팩토링

--- app/src/main/java/com/project/somnium/MakeImageActivity.kt
+++ app/src/main/java/com/project/somnium/MakeImageActivity.kt
@@ -1,38 +1,31 @@
 package com.project.somnium
 
 import android.os.Bundle
-import android.view.WindowManager
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.project.somnium.databinding.ActivityMakeImageBinding
 import com.project.somnium.makeImg_Recycler.Adapter
 import com.project.somnium.makeImg_Recycler.RecyclerDataModel
-import com.project.somnium.retrofit.GptRepository
-
+import com.project.somnium.viewModel.MakeImageViewModel
 
 class MakeImageActivity : AppCompatActivity() {
-    private lateinit var inputText: String
-    private val linearLayoutManager = LinearLayoutManager(this)
+
+    private lateinit var binding: ActivityMakeImageBinding
+    private lateinit var recyclerAdapter: Adapter
+    private val recyclerDataModel = ArrayList<RecyclerDataModel>()
+    private lateinit var linearLayoutManager: LinearLayoutManager
+
+    private val viewModel: MakeImageViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMakeImageBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
+        binding = ActivityMakeImageBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val recyclerDataModel = ArrayList<RecyclerDataModel>()
-        val recyclerAdapter = Adapter(recyclerDataModel)
+        linearLayoutManager = LinearLayoutManager(this)
 
         binding.chatRecyclerView.apply {
             addItemDecoration(
@@ -42,30 +35,33 @@ class MakeImageActivity : AppCompatActivity() {
                 )
             )
             layoutManager = linearLayoutManager
+            recyclerAdapter = Adapter(recyclerDataModel)
             adapter = recyclerAdapter
         }
 
+        observeViewModel()
+
         binding.btnSend.setOnClickListener {
-            inputText = binding.etDreamInput.text.toString()
+            val inputText = binding.etDreamInput.text.toString()
 
             if (inputText.isNotBlank()) {
                 binding.etDreamInput.text.clear()
-                Toast.makeText(this,"이미지 생성중입니다.",Toast.LENGTH_SHORT).show()
-                val request = GptRepository(inputText, object : GptRepository.ApiCallback  {
-                    override fun onDataReceived(data: RecyclerDataModel) {
-                        recyclerDataModel.add(data)
-                        recyclerAdapter.notifyDataSetChanged()
-                    }
-                })
-
-                request.gptRequest()
+                Toast.makeText(this, "이미지 생성중입니다.", Toast.LENGTH_SHORT).show()
+                viewModel.generateImage(inputText)
             } else {
                 Toast.makeText(this, "꿈 내용을 입력해주세요!", Toast.LENGTH_SHORT).show()
             }
-
         }
-
-
     }
 
+    private fun observeViewModel() {
+        viewModel.imageData.observe(this) { data ->
+            recyclerDataModel.add(data)
+            recyclerAdapter.notifyDataSetChanged()
+        }
+
+        viewModel.errorMessage.observe(this) { message ->
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+    }
 }

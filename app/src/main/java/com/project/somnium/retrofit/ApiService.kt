1c0f4be871a21f05159bb77116bfda8e5cae32d7
junho <junho5508@naver.com>
2026-04-23 21:57:24 +0900

    ApiService 내 GPT 이미지 생성 API 호출 방식을 Coroutines 연동을 위해 suspend 함수 및 Response 형태로 변경

--- app/src/main/java/com/project/somnium/retrofit/ApiService.kt
+++ app/src/main/java/com/project/somnium/retrofit/ApiService.kt
@@ -1,6 +1,6 @@
 package com.project.somnium.retrofit
 
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
@@ -8,9 +8,8 @@ import retrofit2.http.POST
 interface ApiService {
 
     @POST("v1/images/generations")
-    fun postGptApiImage(
+    suspend fun postGptApiImage(
         @Header("Authorization") authHeader: String,
         @Body requestBody: ImageRequestBody
-    ): Call<ApiDataModel?>?
-
+    ): Response<ApiDataModel>
 }

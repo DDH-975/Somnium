41e3a3f079b0ac09199f28cff8e2815bba2f7957
junho <junho5508@naver.com>
2026-04-30 18:44:41 +0900

    refactor: Repository 패턴을 적용한 ViewModel 데이터 처리 로직 개선

--- app/src/main/java/com/project/somnium/viewModel/WriteDiaryViewModel.kt
+++ app/src/main/java/com/project/somnium/viewModel/WriteDiaryViewModel.kt
@@ -2,11 +2,34 @@ package com.project.somnium.viewModel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import com.project.somnium.diaryDb.DataBase
+import com.project.somnium.diaryDb.DiaryDataClass
 import com.project.somnium.diaryDb.DiaryRepository
+import kotlinx.coroutines.launch
 
-class WriteDiaryViewModel(application: Application): AndroidViewModel(application) {
+class WriteDiaryViewModel(application: Application) : AndroidViewModel(application) {
     private val db = DataBase.getDatabase(application)
     private val diaryDao = db.DiaryDataDao()
     private val repo = DiaryRepository(diaryDao)
+
+    private lateinit var _selectByIdData: LiveData<DiaryDataClass>
+    val selectByIdData = _selectByIdData
+
+    fun insertData(data: DiaryDataClass) {
+        viewModelScope.launch {
+            repo.insertData(data)
+        }
+    }
+
+    fun updateById(id: Int, title: String, content: String, imgUrl: String) {
+        viewModelScope.launch {
+            repo.updateByID(id, title, content, imgUrl)
+        }
+    }
+
+    fun selectById(id: Int) {
+        _selectByIdData = repo.selectByID(id)
+    }
 }

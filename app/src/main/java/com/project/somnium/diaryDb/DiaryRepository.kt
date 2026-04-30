9890b563784f7bf2a4061c9f3423a62f58d59c57
junho <junho5508@naver.com>
2026-04-30 18:41:36 +0900

    refactor: DiaryRepository 내 데이터 삽입, 수정 및 ID별 조회 기능 추가

--- app/src/main/java/com/project/somnium/diaryDb/DiaryRepository.kt
+++ app/src/main/java/com/project/somnium/diaryDb/DiaryRepository.kt
@@ -1,12 +1,23 @@
 package com.project.somnium.diaryDb
 
 import androidx.lifecycle.LiveData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class DiaryRepository (private val dao: DiaryDao) {
-    private lateinit var  thumbnailData: LiveData<List<DiaryDataClass>>
+class DiaryRepository(private val dao: DiaryDao) {
+    fun getDataDesc(): LiveData<List<DiaryDataClass>> = dao.getDataDesc()
 
-    fun getDataDesc(): LiveData<List<DiaryDataClass>>{
-        thumbnailData = dao.getDataDesc()
-        return dao.getDataDesc()
+    suspend fun insertData(data: DiaryDataClass) {
+        withContext(Dispatchers.IO) {
+            dao.insertData(data)
+        }
     }
+
+    suspend fun updateByID(id: Int, title: String, content: String, imgUrl: String) {
+        withContext(Dispatchers.IO) {
+            dao.updateByID(id, title, content, imgUrl)
+        }
+    }
+
+    fun selectByID(id: Int): LiveData<DiaryDataClass> = dao.selectByID(id)
 }

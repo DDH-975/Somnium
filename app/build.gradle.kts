e1380961788f18f2e9288375b783bd461e3a0e3c
junho <junho5508@naver.com>
2026-05-06 19:02:47 +0900

    build: activity-ktx 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.15.1")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.8.x")
     annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
     kapt("com.github.bumptech.glide:compiler:4.15.1")
     kapt("androidx.room:room-compiler:2.6.1")

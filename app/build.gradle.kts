35df6d4713e3afb8455ff2482bfe3fababce08a4
junho <junho5508@naver.com>
2026-05-07 21:06:30 +0900

    Hilt 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     kotlin("kapt")
+    alias(libs.plugins.hilt)
 }
 
 val properties = Properties()
@@ -76,6 +77,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.activity:activity-ktx:1.8.x")
     annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
     kapt("com.github.bumptech.glide:compiler:4.15.1")
     kapt("androidx.room:room-compiler:2.6.1")
     annotationProcessor("androidx.room:room-compiler:2.6.1")

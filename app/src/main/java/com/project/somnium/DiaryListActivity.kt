be7a5f42eb362ee896e65f70ab192c85d8968fce
junho <junho5508@naver.com>
2026-05-07 19:23:10 +0900

    refactor: DiaryListActivity 뷰모델 적용 observeViewModel()메서드를 통해 viewmodel data 관찰 및 ui update

--- app/src/main/java/com/project/somnium/DiaryListActivity.kt
+++ app/src/main/java/com/project/somnium/DiaryListActivity.kt
@@ -2,25 +2,24 @@ package com.project.somnium
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.project.somnium.databinding.ActivityDiaryListBinding
-import com.project.somnium.diaryDb.DataBase
 import com.project.somnium.diary_List_Recycler.DiaryListAdapter
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.project.somnium.viewModel.DiaryListViewModel
 
 class DiaryListActivity : AppCompatActivity() {
+    private val viewModel: DiaryListViewModel by viewModels()
+    private lateinit var binding: ActivityDiaryListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val binding = ActivityDiaryListBinding.inflate(layoutInflater)
+        binding = ActivityDiaryListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -28,23 +27,19 @@ class DiaryListActivity : AppCompatActivity() {
             insets
         }
 
-        val db = DataBase.getDatabase(this)
-        val diaryDao = db.DiaryDataDao()
-
         binding.DiaryInventory.apply {
             addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
             layoutManager = LinearLayoutManager(context)
         }
+        observeViewModel()
+    }
 
-        lifecycleScope.launch(Dispatchers.IO) {
-            val data = diaryDao.getAllData()
-
-            withContext(Dispatchers.Main) {
-                val adapter = DiaryListAdapter(data.toMutableList()){ diary -> withContext(Dispatchers.IO){
-                    diaryDao.deleteByTile(diary.title)
-                } }
-                binding.DiaryInventory.adapter = adapter
+    private fun observeViewModel() {
+        viewModel.allData.observe(this) { data ->
+            val adapter = DiaryListAdapter(data.toMutableList()) { diary ->
+                viewModel.deleteByTile(diary.title)
             }
+            binding.DiaryInventory.adapter = adapter
         }
     }
 }

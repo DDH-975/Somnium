ded36bd832ad5709be2e0bfe2be57ca3133a658d
junho <junho5508@naver.com>
2026-05-08 18:45:39 +0900

    refactor: 데이터베이스 모듈 추가

---
+++ app/src/main/java/com/project/somnium/di/DataBaseModule.kt
@@ -0,0 +1,35 @@
+package com.project.somnium.di
+
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import com.project.somnium.diaryDb.DataBase
+import com.project.somnium.diaryDb.DiaryDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataBaseModule {
+
+    @Provides
+    @Singleton
+    fun providesDataBase(@ApplicationContext context: Context): DataBase{
+        return Room.databaseBuilder(
+            context,
+            DataBase::class.java,
+            "DiaryDB"
+        ).build()
+    }
+
+    @Provides
+    fun providesDao(dataBase: DataBase): DiaryDao{
+        return dataBase.DiaryDataDao()
+    }
+
+
+}

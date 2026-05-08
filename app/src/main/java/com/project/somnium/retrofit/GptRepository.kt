10864e692a9ebf029ea58860864c1bb9015b26e4
junho <junho5508@naver.com>
2026-05-08 18:28:53 +0900

    refactor: GptRepository hilt 적용 - GptRepository 의존성 주입 적용

--- app/src/main/java/com/project/somnium/retrofit/GptRepository.kt
+++ app/src/main/java/com/project/somnium/retrofit/GptRepository.kt
@@ -2,30 +2,32 @@ package com.project.somnium.retrofit
 
 import com.project.somnium.BuildConfig
 import com.project.somnium.makeImg_Recycler.RecyclerDataModel
+import javax.inject.Inject
 
-class GptRepository {
-    private val service = NetworkClient.RetrofitClient.getClient().create(ApiService::class.java)
+class GptRepository @Inject constructor(
+    private val service: ApiService
+) {
     private val apikey = "Bearer ${BuildConfig.Apikey}"
 
-        suspend fun requestImage(prompt: String): Result<RecyclerDataModel> {
-            return try {
-                val request = ImageRequestBody(prompt)
-                val response = service.postGptApiImage(apikey, request)
+    suspend fun requestImage(prompt: String): Result<RecyclerDataModel> {
+        return try {
+            val request = ImageRequestBody(prompt)
+            val response = service.postGptApiImage(apikey, request)
 
-                if (response.isSuccessful) {
-                    val result = response.body()
-                    val url = result?.data?.getOrNull(0)?.url
+            if (response.isSuccessful) {
+                val result = response.body()
+                val url = result?.data?.getOrNull(0)?.url
 
-                    if (url != null) {
-                        Result.success(RecyclerDataModel(prompt, url))
-                    } else {
-                        Result.failure(Exception("이미지 URL이 없습니다."))
-                    }
+                if (url != null) {
+                    Result.success(RecyclerDataModel(prompt, url))
                 } else {
-                    Result.failure(Exception("API 호출 실패: ${response.code()}"))
+                    Result.failure(Exception("이미지 URL이 없습니다."))
                 }
-            } catch (e: Exception) {
-                Result.failure(e)
+            } else {
+                Result.failure(Exception("API 호출 실패: ${response.code()}"))
             }
+        } catch (e: Exception) {
+            Result.failure(e)
         }
+    }
 }
